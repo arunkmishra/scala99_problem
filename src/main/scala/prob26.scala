bdbf322175dc4f47fde2c2bf6b639eb407203d6b
Arun <arun@github.com>
2018-03-30 22:20:41 +0530

    prob26

---
+++ src/main/scala/prob26.scala
@@ -0,0 +1,22 @@
+/*scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...*/
+
+object prob26 {
+  def main(args: Array[String]): Unit = {
+    val ls = List(1,2,3)
+
+    println("combination patter : \n " + combinations(2,ls))
+  }
+  
+  def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
+    ls match {
+      case Nil => Nil
+      case sublist@(_ :: tail) => f(sublist) ::: flatMapSublists(tail)(f)
+    }
+
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else flatMapSublists(ls) { sl =>
+      combinations(n - 1, sl.tail) map {sl.head :: _}
+    }
+}
