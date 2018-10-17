789ea0df2e5759159ce166ac38c5bb8dfb3e480a
Arun <arun@github.com>
2018-10-17 15:39:53 +0530

    added changes

---
+++ src/main/scala/prob27.scala
@@ -0,0 +1,9 @@
+/*Prob1 =>scala> group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David, Evi), List(Flip, Gary, Hugo, Ida)), ...*/
+
+/*scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo, Ida)), ...*/
+
+object prob27 {
+
+}
