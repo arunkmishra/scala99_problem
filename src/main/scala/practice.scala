d1c0b2c104d6867f86294bbea30656f4f364cf01
Arun <arun@github.com>
2018-12-24 00:13:33 +0530

    removed unused files

---
+++ src/main/scala/practice.scala
@@ -0,0 +1,26 @@
+object practice extends App {
+  def remove(ls: List[Char]): String = {
+    def oneCheck(c: Char, r: List[Char]): List[Char] = r.headOption match {
+      case Some(x) if x - c == 1 => oneCheck(x, r.tail)
+      case _ => r
+    }
+
+    def reT(acc: List[Char], v: List[Char]): List[Char] = {
+      v match {
+        case h :: s:: tail if (s-h) ==1 => reT(List.empty[Char], acc :::oneCheck(s, tail))
+        case h :: s:: tail if (s-h) != 1 => reT(acc:+h, s::tail)
+        case h:: Nil => reT(acc :+ h, Nil)
+        case _ => acc
+      }
+    }
+    val c = reT(List.empty[Char], ls)
+    c.mkString
+  }
+
+  val ls: List[Char] = "axefyzijkmoq".toList
+
+
+
+  println('b'-'a')
+  println("res : " + remove(ls))
+}
--- src/main/scala/prob26.scala
+++ src/main/scala/prob26.scala
@@ -1,13 +1,27 @@
 /*scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
 res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...*/
+object CombinationOps {
+  implicit class CombinatorialList[A](l: List[A]) {
 
+    def xcombinations(n: Int): List[List[A]] =
+      if(n> l.size)
+        Nil
+      else l match {
+        case _::_ if n == 1 => l.map(List(_))
+        case h :: t => t.xcombinations(n-1).map(h :: _) ::: xcombinations(n)
+        case _ => Nil
+      }
+  }
+}
 object prob26 {
   def main(args: Array[String]): Unit = {
     val ls = List(1,2,3)
+    val obj = new CombinationOps.CombinatorialList(ls)
 
     println("combination patter : \n " + combinations(2,ls))
+    println("combination patter : \n " + obj.xcombinations(2))
   }
-  
+
   def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
     ls match {
       case Nil => Nil
